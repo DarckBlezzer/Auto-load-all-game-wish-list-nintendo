b62b7bf58ea374625ad76302d9f8554137721860
Marco Antonio <ma_siqueiros@hotmail.com>
2021-09-15 12:05:44 -0600

    Update load_all_games_wish_list_nintendo.user.js

--- load_all_games_wish_list_nintendo.user.js
+++ load_all_games_wish_list_nintendo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Show all games nintendo wish list
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.1
 // @description  This userscript help you to load all games that you have in your wish-list
 // @author       DarckBlezzer
 // @match        https://www.nintendo.com/wish-list/*
@@ -15,50 +15,56 @@
 const get_element = () => document.querySelector("wishlist-manager");
 const get_button = () => document.querySelector("styled-button.secondary-blue");
 const get_select = () => document.querySelector("styled-select");
-const get_items_container = () => document.querySelector("ul.items.constrained");
+const get_items_container = () =>
+  document.querySelector("ul.items.constrained");
 
 const check_if_element_has_class = () => {
-    const element = get_element();
-    return element.classList.contains('has-more') && element.classList.contains('has-items');
+  const element = get_element();
+  return (
+    element.classList.contains("has-more") &&
+    element.classList.contains("has-items")
+  );
 };
 
 const check_if_element_has_items = () => {
-    const element = get_element();
-    return element.classList.contains('has-more');
+  const element = get_element();
+  return element.classList.contains("has-more");
 };
 
 const wait_until_has_class = function (fun = () => {}) {
-    setTimeout(() => (check_if_element_has_class() ? fun() : wait_until_has_class(...arguments)), 500);
+  setTimeout(
+    () =>
+      check_if_element_has_class() ? fun() : wait_until_has_class(...arguments),
+    500
+  );
 };
 
 const run_until_not_has_more = function (fun = () => {}) {
-    setTimeout(() => {
-        if(check_if_element_has_items()){
-            fun();
-            run_until_not_has_more(...arguments);
-        }else{
-            console.log("ya no hay items :(")
-        }
-    }, 500);
+  setTimeout(() => {
+    if (check_if_element_has_items()) {
+      fun();
+      run_until_not_has_more(...arguments);
+    } else {
+      console.log("ya no hay items :(");
+    }
+  }, 500);
 };
 
 const show_all = () => {
-   run_until_not_has_more(() => {
-       console.log("Click load more baby :3");
-       const button = get_button();
-       if(button) button.click();
-   });
-}
+  run_until_not_has_more(() => {
+    console.log("Click load more baby :3");
+    const button = get_button();
+    if (button) button.click();
+  });
+};
 
-
-
-window.onload = function() {
-    if(get_element()){
-        console.log("Yes, the element exist... :3")
-        wait_until_has_class(() => {
-            console.log("Good, we start to load more games, baby :3");
-            get_select().onchange = show_all;
-            show_all();
-        });
-    }
+window.onload = function () {
+  if (get_element()) {
+    console.log("Yes, the element exist... :3");
+    wait_until_has_class(() => {
+      console.log("Good, we start to load more games, baby :3");
+      get_select().onchange = show_all;
+      show_all();
+    });
+  }
 };
